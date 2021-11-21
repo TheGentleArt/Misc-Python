666ddfb1518b4911a5d4151ed22b04cc7166d848
TheGentleArt <76894554+TheGentleArt@users.noreply.github.com>
2021-11-21 12:32:41 -0500

    Updated plot
    Added TDC and BDC labels to graphs

--- Engine_Related/ConRod_Force_Motoring.py
+++ Engine_Related/ConRod_Force_Motoring.py
@@ -9,7 +9,7 @@ from matplotlib.gridspec import GridSpec
 # To-do:
 # Ask user for inputs in user input section (once satisfied with script)
 # Want to show g's as well as m/s^2 for accel units in chart (2nd y-axis)
-# Warn user that this does not take into combustion pressures yet
+# Warn user that this does not take into account combustion pressures yet
 # Add in BMEP to force calcs
 # Estimate peak combustion pressure at say 10-15 deg ATDC instead of BMEP?
 # Get consistent with np or not np
@@ -25,6 +25,7 @@ pi = np.pi
 cos = math.cos
 sin = math.sin
 sqrt = math.sqrt
+
 
 # User Inputs ---change this to input() later
 NE = 6500  # Engine Speed (rpm)
@@ -104,12 +105,17 @@ gs = GridSpec(4, 1, figure=fig1)  # Create a grid to be used to place subplots
 major_ticks = np.arange(0,721,90)
 minor_ticks = np.arange(0,721,30)
 
-# Create the first plot, pos vs crank angle
+# Create the first plot
 ax1 = plt.subplot(gs[0, 0])
 plt.plot(CA, position_piston,
          color='c', linewidth=1, label='Distance Wrist Pin from Crank Center')
 plt.plot(CA, position_piston_bdc,
          '-.', color='c', linewidth=1, label='Position from BDC')
+plt.axvline(x=0.5, linestyle='-.', color = 'k', label = 'TDC')
+plt.axvline(x=360, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=720, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=180, linestyle=':', color = 'k', label = 'BDC')
+plt.axvline(x=540, linestyle=':', color = 'k', label = '_nolegned_')
 plt.title("Piston Position vs Crank Angle")
 plt.xlabel("Crank Angle (deg)")
 plt.ylabel("Position (mm)")
@@ -118,12 +124,17 @@ ax1.set_xticks(major_ticks)
 ax1.set_xticks(minor_ticks,minor=True)
 ax1.grid(which="minor", alpha=0.5)
 plt.grid()
-ax1.legend()
+ax1.legend(prop={'size':8})
 
 ax2 = plt.subplot(gs[1, 0])
 plt.plot(CA, velocity_piston, color='r', linewidth=1, label='Velocity')
 plt.plot(CA, velocity_piston_abs,
          '--', color='r', linewidth=1, label='Absolute Velocity')
+plt.axvline(x=0.5, linestyle='-.', color = 'k', label = 'TDC')
+plt.axvline(x=360, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=720, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=180, linestyle=':', color = 'k', label = 'BDC')
+plt.axvline(x=540, linestyle=':', color = 'k', label = '_nolegned_')
 plt.title("Piston Velocity vs Crank Angle")
 plt.xlabel("Crank Angle (deg)")
 plt.ylabel("Velocity (m/s)")
@@ -132,12 +143,17 @@ ax2.set_xticks(major_ticks)
 ax2.set_xticks(minor_ticks,minor=True)
 ax2.grid(which="minor", alpha=0.5)
 plt.grid()
-ax2.legend()
+ax2.legend(prop={'size':8})
 
 ax3 = plt.subplot(gs[2, 0])
 plt.plot(CA, acceleration_piston, color='b', linewidth=1, label='Acceleration')
 plt.plot(CA, acceleration_piston_abs,
          '--', color='b', linewidth=1, label='Abs Acceleration')
+plt.axvline(x=0.5, linestyle='-.', color = 'k', label = 'TDC')
+plt.axvline(x=360, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=720, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=180, linestyle=':', color = 'k', label = 'BDC')
+plt.axvline(x=540, linestyle=':', color = 'k', label = '_nolegned_')
 plt.title("Piston Acceleration vs Crank Angle")
 plt.xlabel("Crank Angle (deg)")
 plt.ylabel("Acceleration (m/s^2)")
@@ -146,11 +162,16 @@ ax3.set_xticks(major_ticks)
 ax3.set_xticks(minor_ticks,minor=True)
 ax3.grid(which="minor", alpha=0.5)
 plt.grid()
-ax3.legend()
+ax3.legend(prop={'size':8})
 
 ax4 = plt.subplot(gs[3, 0])
 plt.plot(CA, force_piston, color='m', linewidth=1, label='Force')
 plt.plot(CA, force_piston_abs, '--', color='m', linewidth=1, label='Abs Force')
+plt.axvline(x=0.5, linestyle='-.', color = 'k', label = 'TDC')
+plt.axvline(x=360, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=720, linestyle='-.', color = 'k', label = '_nolegned_')
+plt.axvline(x=180, linestyle=':', color = 'k', label = 'BDC')
+plt.axvline(x=540, linestyle=':', color = 'k', label = '_nolegned_')
 plt.title("Piston Force vs Crank Angle")
 plt.xlabel("Crank Angle (deg)")
 plt.ylabel("Force (kN)")
@@ -159,7 +180,7 @@ ax4.set_xticks(major_ticks)
 ax4.set_xticks(minor_ticks,minor=True)
 ax4.grid(which="minor", alpha=0.5)
 plt.grid()
-ax4.legend()
+ax4.legend(prop={'size':8})
 
 plt.tight_layout()
 plt.show()
